43c1e0511fbd1cee91018bb1b8cec0af85b9129e
Jordi Farré <jordi.farre@mango.com>
2018-02-04 20:17:50 +0100

    test refactor

--- src/ReserveController.js
+++ src/ReserveController.js
@@ -9,9 +9,11 @@ module.exports = class ReserveController {
   reserve(request, response) {
     const reservation = new Reservation(request.body.train_id, request.body.number_seats);
     this.reserveService.reserve(reservation, (reservationResponse) => {
-      response.status(200).send({"train_id": reservationResponse.trainId, 
-                                  "booking_reference": reservationResponse.bookingReference,
-                                  "seats": reservationResponse.seats});
+      response
+        .status(200)
+        .send({"train_id": reservationResponse.trainId, 
+              "booking_reference": reservationResponse.bookingReference,
+              "seats": reservationResponse.seats});
     });
   }
 
--- src/test/ReserveController.test.js
+++ src/test/ReserveController.test.js
@@ -1,18 +1,17 @@
 const ReserveController = require("../ReserveController");
 const ReserveService = require("../ReserveService");
+jest.mock("../ReserveService");
 const Reservation = require("../Reservation");
 const ReservationResponse = require("../ReservationResponse")
 const mockRes = require('jest-mock-express').response;
+const expectedReservation = new Reservation("express_2000", 2);
+const reserveService = new ReserveService();
+const reserveSpy = reserveService.reserve; 
+const reserveController = new ReserveController(reserveService);
+const request = {"body": {"train_id": "express_2000", "number_seats": 2}};
+const response = mockRes();
 
 test('should reserve seats on train', () => {
-  const expectedReservation = new Reservation("express_2000", 2);
-  const reserveService = new ReserveService();
-  const reserveController = new ReserveController(reserveService);
-  const reserveSpy = jest.fn(); 
-  reserveService.reserve = reserveSpy;
-  const request = {"body": {"train_id": "express_2000", "number_seats": 2}};
-  const response = mockRes();
-
   reserveController.reserve(request, response);
   reserveSpy.mock.calls[0][1](new ReservationResponse("express_2000", "75bcd15", ["1A", "1B"]))
 
